25bb3f33312ef1738996bc459ebb0137722d5d03
Wiltrouble <wiltrouble@gmail.com>
2021-02-20 00:14:49 -0400

    Home screen completed

--- src/screens/homeScreem/index.tsx
+++ src/screens/homeScreem/index.tsx
@@ -8,7 +8,8 @@ const HomeScreen = () => {
     return (
         <View style={styles.root}>
             <Image style={styles.image} source={image}/>
-            <Text>Hme screem</Text>
+            <Text style={styles.header1}>Welcome to Vcrypto</Text>
+            <Text style={styles.header2}>Invest your virtual $100.000 and compete with others</Text>
 
         </View>    
     )
--- src/screens/homeScreem/styles.ts
+++ src/screens/homeScreem/styles.ts
@@ -2,11 +2,25 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
     root: {
-
+        padding: 20,
+        alignItems: 'center',
+        flex: 1,
+        backgroundColor: 'white',
     },
     image: {
-        height: 400,
-        width: 300,
+        height: '60%',
+        aspectRatio: 1,
+    },
+    header1: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 50,
+        marginBottom: 20,
+    },
+    header2: {
+        fontSize: 20,
+        textAlign: 'center',
+        color: '#707070',
     }
 
 })
